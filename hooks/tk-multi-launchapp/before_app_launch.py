9a380b6b5ab695fca8edcd73993eb4fdf524ac44
Anthony Kramer <anthony@chicken-bone.com>
2018-10-12 14:24:02 -0500

    set up a tk-nuke-render engine first stab at creating a tk-nuke-redner engine to be used on the farm to limit what toolkit apps get loaded since we dont need many of them just for farm jobs. Also set the NUKE_PATH to dev mode for testing.

--- hooks/tk-multi-launchapp/before_app_launch.py
+++ hooks/tk-multi-launchapp/before_app_launch.py
@@ -53,8 +53,8 @@ class BeforeAppLaunch(tank.Hook):
         if engine_name == "tk-nuke":
             self.logger.debug("[CBFX] nuke tools: %s" % self.sgtk.roots["nuke_tools"])
             env_vars = {
-                # "NUKE_PATH": "R:\\code\\work\\anthony.kramer\\nuke\\cbfx-nuke-tools;%s" % self.sgtk.roots['nuke_tools'],
-                "NUKE_PATH": "S:\\tools\\nuke\\cbfx\\current;%s" % self.sgtk.roots['nuke_tools'],
+                "NUKE_PATH": "R:\\code\\work\\anthony.kramer\\nuke\\cbfx-nuke-tools;%s" % self.sgtk.roots['nuke_tools'],
+                # "NUKE_PATH": "S:\\tools\\nuke\\cbfx\\current;%s" % self.sgtk.roots['nuke_tools'],
             }
             for k, v in env_vars.iteritems():
                 tank.util.append_path_to_env_var(k, v)
